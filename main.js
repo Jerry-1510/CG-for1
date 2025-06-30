0e642b41adde6c4ff226bccb0b8ea1d548b54c54
TUAN <btuancreator@gmail.com>
2025-06-30 09:41:56 +0700

    sửa lần 1

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 for (let i = 0; i < 5; i++) {
   document.writeln("The number is N");
 }
+
+document.getElementById("nhap").addEventListener("input", function () {
+  let nhap = +document.getElementById("nhap").value;
+  let total = document.getElementById("total");
+  let count = 0;
+  for (let i = 1; i < nhap; i++) {
+    count += i;
+  }
+
+  total.innerHTML = count;
+});
